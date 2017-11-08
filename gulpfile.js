d89dab0b4f32c9cf2c9073906500758de262134e
Pete van Blerk <pvanblerk@neworbit.co.uk>
2017-11-08 17:21:39 +0000

    Add build step to gulpfile for vsts

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,18 @@ var browserSync = require('browser-sync').create();
 // If you don't use bundle:
 // gulp.task('build', shell.task(['jekyll serve']));
 // If you use  Windows Subsystem for Linux (thanks @SamuliAlajarvela):
-gulp.task('build', shell.task(['bundle exec jekyll serve --force_polling']));
+gulp.task('vsts-build', shell.task(['bundle exec jekyll build']));
+
+gulp.task('jk-serve', shell.task(['bundle exec jekyll serve --force_polling']));
 
 // Task for serving blog with Browsersync
-gulp.task('serve', function() {
+gulp.task('bs-serve', function() {
     browserSync.init({ server: { baseDir: '_site/' } });
     // Reloads page when some of the already built files changed:
     gulp.watch('_site/**/*.*').on('change', browserSync.reload);
 });
 
-gulp.task('default', ['build', 'serve']);
+gulp.task('default', ['jk-serve', 'bs-serve']);
 
 gulp.task('test', () => {
     console.log("gulp script running");
